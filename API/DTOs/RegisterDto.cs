f3780adf616a693ccafc4682e8e039b904eb2dc5
Mike Mavrogiannis <mixmavr@hotmail.com>
2025-02-05 00:12:19 +0200

    Error handling

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -8,10 +8,11 @@ namespace API.DTOs
 {
     public class RegisterDto
     {
-        [Required(ErrorMessage = "Username is required")]
-        public required string Username {get; set;}
+        [Required]
+        public string Username {get; set;} = string.Empty;
 
-        [Required(ErrorMessage = "Password is required")]
-        public required string Password {get; set;}
+        [Required]
+        [StringLength(8, MinimumLength = 4)]
+        public string Password {get; set;} = string.Empty;
     }
 }
